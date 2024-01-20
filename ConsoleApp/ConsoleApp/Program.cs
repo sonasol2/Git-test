881891d971156fea11f3ce772aa747fd015442fb
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-01-21 00:18:11 +0200

    Last Work

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,7 +3,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello there, World!");
+        Console.Write("enter your name:");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Hello,{name}");
+
+        string? name2;
+        name = "Rob";
+
+        name2 = name;
+	    
+
     }
 }
-
