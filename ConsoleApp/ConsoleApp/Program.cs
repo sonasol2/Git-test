bebe9f3ddf78b51d20b6e7fc05a5263433698c4e
sonasol2 <serjkon93@yandex.ru>
2024-01-20 17:15:55 +0200

    Add ConsoleApp sln

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
